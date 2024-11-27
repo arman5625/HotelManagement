24a62cd30a43014751def5a9dc5f89ef33429c85
arman5625 <108667833+arman5625@users.noreply.github.com>
2024-11-27 22:40:01 +0530

    webhook environment variable changed

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import { createBooking, updateHotelRoom } from "@/src/libs/api";
 
 const checkoout_session_completed = 'checkout.session.completed';
 
-const stripe = new Stripe(process.env.stripe_secret_key as string, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: '2024-11-20.acacia',
 });
 
