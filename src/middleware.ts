d4c206ade67438dfe6729569aac8725a0d9e99ca
arman5625 <108667833+arman5625@users.noreply.github.com>
2024-12-07 01:22:26 +0530

    stripe webhook redirect issue solveD

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export {default} from "next-auth/middleware"
 
 export const config ={
-    matcher: ["/users/:path*", "/api/:path*"],
+    matcher: ["/users/:path*", "/api/(?!webhook)(:path*)"],
 
 }
